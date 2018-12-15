c86658926909d528ca355b7ef5856f771865bbe8
phs1234 <32615067+phs1234@users.noreply.github.com>
2018-12-15 14:25:52 +0900

    #2 Bluetooth test

---
+++ CocktailArduinoApp/CocktailArduinoApp.ino
@@ -0,0 +1,16 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2, 3);   //bluetooth module Tx:Digital 2 Rx:Digital 3
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+  Serial.println("ATcommand");  //ATcommand Start
+}
+
+void loop() {
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+}
