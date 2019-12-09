bf6e29398b455cee6cbfea917460efa3fc6cfe6b
phs1234 <timemarket777@naver.com>
2019-12-10 00:59:53 +0900

    슬라이드 완성

--- CocktailMobileApp/app/src/main/java/com/example/administrator/cocktailmobileapp/SlideFragment.java
+++ CocktailMobileApp/app/src/main/java/com/example/administrator/cocktailmobileapp/SlideFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class SlideView extends Fragment {
+public class SlideFragment extends Fragment {
     private static final String TAG = "SlideView";
 
     private TextView mName;
@@ -24,6 +25,24 @@ public class SlideView extends Fragment {
     private Button mButton;
     private ImageView mCardImage;
     private RelativeLayout mBackground;
+
+    public static SlideFragment createSlideFragment (String name, String title, String description, int imageID , GradientDrawable.Orientation gradientAngle, int gradientStartColor, int gradientEndColor, int position) {
+        SlideFragment slideFragment = new SlideFragment();
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString("name", name);
+        bundle.putString("title", title);
+        bundle.putString("description", description);
+        bundle.putInt("imageID", imageID);
+        bundle.putInt("gradientStartColor", gradientStartColor);
+        bundle.putInt("gradientEndColor", gradientEndColor);
+        bundle.putInt("position", position);
+
+        slideFragment.setArguments(bundle);
+
+        return slideFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,30 +55,32 @@ public class SlideView extends Fragment {
         mCardImage = view.findViewById(R.id.card_image);
         mBackground = view.findViewById(R.id.background);
 
+        Bundle bundle = getArguments();
+
+        mName.setText(bundle.getString("name"));
+        mTitle.setText(bundle.getString("title"));
+        mDesciption.setText(bundle.getString("description"));
+        mCardImage.setImageBitmap(getRoundedBitmap(bundle.getInt("imageID")));
+
+        GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.BL_TR, new int[] {getResources().getColor(R.color.colorGreen), getResources().getColor(R.color.colorWhite)});
+        gradientDrawable.setCornerRadius(0f);
+        mBackground.setBackgroundDrawable(gradientDrawable);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         return view;
-    }
-
-    public void setNameText (String text) { mName.setText(text); }
-
-    public void setTitleText (String text) { mTitle.setText(text); }
-
-    public void setDesciptionText (String text) {
-        mDesciption.setText(text);
-    }
-
-    public void setBackgroundImage (int imageId) { mCardImage.setImageBitmap(getRoundedBitmap(imageId)); }
-
-    public void setBackgroudGradient (GradientDrawable gradientDrawable) { mBackground.setBackgroundDrawable(gradientDrawable);}
-
-    public Button getButton() {
-        return mButton;
     }
 
     private Bitmap getRoundedBitmap(int imageId) {
         Bitmap bitmap = BitmapFactory.decodeResource( getResources(), imageId);
         RoundedBitmapDrawable roundedDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
 
-        roundedDrawable.setCornerRadius(32);
+        roundedDrawable.setCornerRadius(64);
         roundedDrawable.setAntiAlias(true);
 
         return roundedDrawable.getBitmap();
